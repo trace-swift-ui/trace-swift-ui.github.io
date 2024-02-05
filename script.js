25b9cc1effed8e547b4fae4d78368836e68648e8
Vidyadhar Thatte <vidy.thatte@gmail.com>
2024-02-04 22:03:48 -0800

    updates

--- script.js
+++ script.js
@@ -1,12 +1,127 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // Get the modal
-    var modal = document.getElementById('myModal');
+    var modal = document.getElementById('moreAboutXcodeModal');
 
     // Get the button that opens the modal
-    var btn = document.getElementById('openModalBtn');
+    var btn = document.getElementById('openMoreAboutXcodeBtn');
 
     // Get the close button
     var closeButton = document.querySelector('.close-button');
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+        modal.classList.remove('hidden');
+    }
+
+    // When the user clicks on the close button, close the modal
+    closeButton.addEventListener('click', function() {
+        modal.classList.add('hidden');
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.classList.add('hidden');
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the modal
+    var modal = document.getElementById('moreAboutDataModelModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById('openMoreAboutDataModelBtn');
+
+    // Get the close button
+    var closeButton = document.querySelector('.close-datamodal-button');
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+        modal.classList.remove('hidden');
+    }
+
+    // When the user clicks on the close button, close the modal
+    closeButton.addEventListener('click', function() {
+        modal.classList.add('hidden');
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.classList.add('hidden');
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the modal
+    var modal = document.getElementById('moreAboutSwiftUIModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById('openMoreAboutSwiftUIBtn');
+
+    // Get the close button
+    var closeButton = document.querySelector('.close-swiftui-button');
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+        modal.classList.remove('hidden');
+    }
+
+    // When the user clicks on the close button, close the modal
+    closeButton.addEventListener('click', function() {
+        modal.classList.add('hidden');
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.classList.add('hidden');
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the modal
+    var modal = document.getElementById('moreAboutActionsModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById('openMoreAboutActionsBtn');
+
+    // Get the close button
+    var closeButton = document.querySelector('.close-actions-button');
+
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+        modal.classList.remove('hidden');
+    }
+
+    // When the user clicks on the close button, close the modal
+    closeButton.addEventListener('click', function() {
+        modal.classList.add('hidden');
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.classList.add('hidden');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get the modal
+    var modal = document.getElementById('moreAboutDevAccountModal');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById('openMoreAboutDevAccountBtn');
+
+    // Get the close button
+    var closeButton = document.querySelector('.close-dev-button');
 
     // When the user clicks on the button, open the modal 
     btn.onclick = function() {
